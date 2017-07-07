6adbf76a8ac0e3840c81946351f9404d2e5d6a1b
Manisha W <mwann031@uottawa.ca>
2017-07-07 14:49:27 -0400

    Manisha changes, animations and start level 3

---
+++ Assets/scripts/LevelSelectScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void level1OnClick(){
+		Application.LoadLevel("MainMenu");
+	}
+}
