5ffd974de77600d18690861ac988c8be3cc16cdd
Lucia <lucia.okeh@gmail.com>
2017-07-09 19:52:28 -0400

    camera boundaries finished!

---
+++ Assets/scripts/groundCheck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundCheck : MonoBehaviour {
+
+	private playerController player;
+
+	void Start() {
+		player = gameObject.GetComponentInParent<playerController>();
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D coll){
+
+		player.grounded = false;
+	}
+
+	void OnTriggerStay2D(Collider2D coll)
+	{
+
+		player.grounded = true;
+	}
+
+}
