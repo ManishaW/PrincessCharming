78c61a642b77d1986bada9a65c26572b3d13a4aa
Manisha W <mwann031@uottawa.ca>
2017-07-14 16:46:27 -0400

    Manisha level 3

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	Rigidbody2D myBody;
+	Transform myTrans;
+	float myWidth;
+	public float speed;
+	public LayerMask enemyMask; 
+
+	// Use this for initialization
+	void Start () {
+		myTrans = this.transform;
+		myBody = this.GetComponent<Rigidbody2D>();
+		myWidth = this.GetComponent<SpriteRenderer> ().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 linecastPos = myTrans.position - myTrans.right * (myWidth/2);
+		Debug.DrawLine(linecastPos, linecastPos + Vector2.down);
+		Debug.DrawLine(linecastPos, linecastPos + Vector2.right);
+		Debug.DrawLine(linecastPos, linecastPos + Vector2.left);
+
+
+		bool isGrounded = Physics2D.Linecast (linecastPos, linecastPos + Vector2.down, enemyMask);
+		//Debug.Log (Physics2D.Linecast (linecastPos, linecastPos + Vector2.right, enemyMask));
+
+		if (!isGrounded) {
+			Vector3 currRot = myTrans.eulerAngles;
+			currRot.y += 180;
+			myTrans.eulerAngles = currRot;
+		}
+
+	//Always move forward
+		Vector2 myVel = myBody.velocity;
+		myVel.x = -myTrans.right.x * speed;
+		myBody.velocity = myVel;
+
+	}
+}
