72cd8d044c392e8e54a5bfa68360bac41590c0ec
Lucia <lucia.okeh@gmail.com>
2017-07-16 13:02:05 -0400

    tutorial and level1 stuff

---
+++ Assets/scripts/tutorialScripts/jumpStop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpStop : MonoBehaviour {
+
+	public GameObject jumpPanel;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void onClose() {
+
+		jumpPanel.SetActive (false);
+		Destroy (this.gameObject);
+		Time.timeScale = 1;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+
+		jumpPanel.SetActive(true);
+		Time.timeScale = 0.0f;
+	}
+}
