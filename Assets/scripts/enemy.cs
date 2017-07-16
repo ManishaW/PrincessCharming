3f210788f3912497ffcba4a80846c05534568234
Manisha W <mwann031@uottawa.ca>
2017-07-16 13:30:47 -0400

    enemy updated

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -8,10 +8,6 @@ public class enemy : MonoBehaviour {
 	float myWidth;
 	public float speed;
 	public LayerMask enemyMask; 
-	public float speed2 = 0.5f;
-	public bool enemyTypeAboveGround;
-	public Vector3 pos1;
-	public Vector3 pos2;
 
 	// Use this for initialization
 	void Start () {
@@ -22,11 +18,10 @@ public class enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (enemyTypeAboveGround) {
-			Vector2 linecastPos = myTrans.position - myTrans.right * (myWidth / 2);
+			Vector2 linecastPos = myTrans.position - myTrans.right * (myWidth);
 			Debug.DrawLine (linecastPos, linecastPos + Vector2.down);
-			Debug.DrawLine (linecastPos, linecastPos + Vector2.right * (myWidth / 3));
-			Debug.DrawLine (linecastPos, linecastPos + Vector2.left * (myWidth / 3));
+			Debug.DrawLine (linecastPos, linecastPos + Vector2.right * (myWidth / 2));
+			Debug.DrawLine (linecastPos, linecastPos + Vector2.left * (myWidth / 2));
 
 
 			bool isGrounded = Physics2D.Linecast (linecastPos, linecastPos + Vector2.down, enemyMask);
@@ -42,19 +37,7 @@ public class enemy : MonoBehaviour {
 			Vector2 myVel = myBody.velocity;
 			myVel.x = -myTrans.right.x * speed;
 			myBody.velocity = myVel;
-		} else {
-			Debug.Log (pos1 + " " + pos2);
-
-			transform.position = Vector3.Lerp (myTrans.position+pos1, myTrans.position+pos2, Mathf.PingPong(Time.time*speed2, 1.0f));
-			Debug.Log (transform.position.x);
-			if (transform.position.x < 1.24) {
-				transform.localScale = new Vector3 (0.1274577f, 0.1274577f, 0.1274577f);
-			} else if (transform.position.x > 2.4) {
-				transform.localScale = new Vector3 (-0.1274577f, 0.1274577f, 0.1274577f);
-
-			
-			
-		}
-	}
+		
+	
 }
 }
