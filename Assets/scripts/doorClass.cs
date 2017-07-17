3233d51e6f21335fd2846ae60b263f5a2065ba5b
Lucia <lucia.okeh@gmail.com>
2017-07-17 15:12:57 -0400

    door class?

--- Assets/scripts/doorClass.cs
+++ Assets/scripts/doorClass.cs
@@ -15,26 +15,25 @@ public class doorClass : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll){
 		//Debug.Log (Application.loadedLevelName);
-		if (player.getKey()) {
+		if (player.getKey ()) {
 			//Door animation?
 			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-			levelPassedCanvas.SetActive(true);
+			levelPassedCanvas.SetActive (true);
 			Time.timeScale = 0.0f;
 
-<<<<<<< HEAD
-			if (SceneManager.GetActiveScene().buildIndex == 4) {
+			if (SceneManager.GetActiveScene ().buildIndex == 4) {
 				LevelSelectScript.level4Complete = true;
-=======
-			if (Application.loadedLevelName == "Level1") {
-				LevelSelectScript.level1Complete = true;
-			}
-			if (Application.loadedLevelName == "Level3") {
-				LevelSelectScript.level3Complete = true;
-			}
-			if (Application.loadedLevelName == "Game") {
-				LevelSelectScript.level2Complete = true;
->>>>>>> 80966e97b10d1b84cd9f4d203f33e57d7f5c35a6
+
+				if (Application.loadedLevelName == "Level1") {
+					LevelSelectScript.level1Complete = true;
+				}
+				if (Application.loadedLevelName == "Level3") {
+					LevelSelectScript.level3Complete = true;
+				}
+				if (Application.loadedLevelName == "Game") {
+					LevelSelectScript.level2Complete = true;
+				}
 			}
 		}
 	}
-}
+	}
