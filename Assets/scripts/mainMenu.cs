52fbbe742ee1bf7f9927134cb1114c6d2e44b56a
Lucia <lucia.okeh@gmail.com>
2017-07-17 23:04:26 -0400

    main menu stuff

--- Assets/scripts/mainMenu.cs
+++ Assets/scripts/mainMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class mainMenu : MonoBehaviour {
 
 	public GameObject settingsPanel;
 	public GameObject helpPanel;
+	public float currVol;
+	public Slider volSlider;
 
 	public void onPlay(){
 		SceneManager.LoadScene (1);
@@ -27,6 +30,12 @@ public class mainMenu : MonoBehaviour {
 	public void onCloseHelp() {
 		helpPanel.SetActive (false);
 	}
-	
+
+	public void updated(){
+		GameObject music = GameObject.FindGameObjectWithTag("gameMusic"); 
+		bgmVolume bVol = (bgmVolume)music.GetComponent (typeof(bgmVolume));
+		currVol = volSlider.value;
+		bVol.SetVolume (currVol);
+	}
 
 }
